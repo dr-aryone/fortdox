9b91878d0bb0e3eb37b03b6cb5af9708d040e7d8
Edgeguide User <edgeguide@Edgeguides-MacBook-Pro.local>
2017-06-13 09:03:15 +0200

    Fixed linting issues

--- app/main.js
+++ app/main.js
@@ -1,22 +1,21 @@
-const {app, BrowserWindow} = require('electron')
-const requestor = require('@edgeguideab/requestor')
-const path = require('path')
-const url = require('url')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
-let win
+let win;
 
 async function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600});
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   win.on('closed', () => {
-    win = null
-  })
+    win = null;
+  });
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
