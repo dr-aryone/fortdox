1ffffb976b0878c2cf896477f83ebfa6d397fb59
Edgeguide User <edgeguide@Edgeguides-MacBook-Pro.local>
2017-06-13 09:03:15 +0200

    Fixed linting issues

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-  res.send({message: "Hello world"})
-})
+  res.send({message: 'Hello world'});
+});
 
-app.listen(8000,  () => {
+app.listen(8000, () => {
   console.log('listening to port: 8000');
-})
+});
