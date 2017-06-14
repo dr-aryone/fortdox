df0d96a04013aaca60c70971b47319ac689b0e2c
Edgeguide User <edgeguide@Edgeguides-MacBook-Pro.local>
2017-06-14 15:27:05 +0200

    Added React and Redux devtools to Electron

--- app/main.js
+++ app/main.js
@@ -1,11 +1,13 @@
 const {app, BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
+const { default: installExtension, REDUX_DEVTOOLS } = require('electron-devtools-installer');
 
 let win;
 
 async function createWindow() {
   win = new BrowserWindow({width: 800, height: 600});
+  installExtension(REDUX_DEVTOOLS);
 
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
