1e9e21507484bc3645371cadc738ac8640fb1b49
Edgeguide User <edgeguide@Edgeguides-MacBook-Pro.local>
2017-06-14 14:30:44 +0200

    Experimental work with react and node/express

--- app/js/index.jsx
+++ app/js/index.jsx
@@ -1,10 +1,56 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const requestor = require('@edgeguideab/requestor');
 
-const Hello = (props) => {
+
+
+class NameForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value:'',
+      name: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    let user = this.state.value;
+    let response = await requestor.post('http://localhost:8000/user', {
+      body: {msg: user}
+    });
+    this.setState({name: response.body.message});
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Namn:
+          <input type='text' value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type='submit' value='skicka' />
+        <User name={this.state.name} />
+      </form>
+    );
+  }
+
+}
+
+const User = ({name}) => {
   return (
-    <h1>Hello world</h1>
+    <h3>Hello, {name}</h3>
   );
 };
 
-ReactDOM.render(<Hello text='hello world' />, document.getElementById('hello'));
+
+ReactDOM.render(<NameForm />, document.getElementById('Form'));
--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send({message: 'Hello world'});
+});
+
+app.get('/test', (req, res) => {
+  res.send({message: 'It is working!'});
+});
+
+app.post('/try', (req, res) => {
+  console.log(req.body);
+  res.send();
+});
+
+app.post('/user', (req, res) => {
+  console.log('The users name is: ' + req.body.msg);
+  res.send({message: req.body.msg + ' was registered!'});
 });
 
 app.listen(8000, () => {
