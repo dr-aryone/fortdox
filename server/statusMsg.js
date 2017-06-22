9897040eb99c141e91725e95fef526d1beb72f9a
Edgeguide User <edgeguide@Edgeguides-MacBook-Pro.local>
2017-06-22 10:07:55 +0200

    Changed server errors, they are now sent as HTTP status codes

---
+++ server/statusMsg.js
@@ -0,0 +1,11 @@
+
+
+const statusMsg = {
+  200: 'Ok',
+  401: 'Wrong password',
+  404: 'User does not exist',
+  409: 'User already exists',
+  500: 'Internal server error'
+};
+
+module.exports = statusMsg;
