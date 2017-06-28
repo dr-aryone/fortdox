75df87b31ab5850139994fb2ac7f6e20fc70172a
Lili Du <lilidu@Lilis-MacBook-Pro.local>
2017-06-28 10:59:25 +0200

    Removed unnecessary texts from SearchItem

--- app/js/components/search/SearchItem.jsx
+++ app/js/components/search/SearchItem.jsx
@@ -3,10 +3,7 @@ const React = require('react');
 const SearchItem = ({title, text, onUpdate}) => {
   return (
     <div>
-      <br />
-      Title:
       <h2>{title}</h2>
-      Text:
       <p>{text}</p>
       <button onClick={onUpdate}>
         Edit
