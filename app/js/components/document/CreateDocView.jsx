56ed4e8fbab19c1f49c9cb8d59b1d6e421b73b7b
Lili Du <lilidu@Lilis-MacBook-Pro.local>
2017-07-04 09:58:28 +0200

    Small updates

--- app/js/components/document/CreateDocView.jsx
+++ app/js/components/document/CreateDocView.jsx
@@ -5,7 +5,7 @@ const CreateDocView = ({input, onChange, onCreate}) => {
   return (
     <div className='container-fluid'>
       <div className='col-sm-10 col-sm-offset-1'>
-        <Form header='Update Document' input={input} onChange={onChange}>
+        <Form header='Create Document' input={input} onChange={onChange}>
           <br />
           <a onClick={onCreate} className='btn'>Create</a>
         </Form>
