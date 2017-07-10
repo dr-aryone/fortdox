730a69f51d691c2329ef83f41d09c50c91f6434d
Lili Du <lilidu@Lilis-MacBook-Pro.local>
2017-07-10 09:56:42 +0200

    New front page on user view

--- app/js/containers/user/UserViewContainer.jsx
+++ app/js/containers/user/UserViewContainer.jsx
@@ -1,15 +1,19 @@
 const {connect} = require('react-redux');
 const UserView = require('components/user/UserView');
+const action = require('actions');
 
 const mapStateToProps = state => {
   return {
-    username: state.login.get('username'),
     currentView: state.navigation.get('currentView')
   };
 };
 
-const mapDispatchToProps = () => {
-  return {};
+const mapDispatchToProps = dispatch => {
+  return {
+    changeView: (nextView) => {
+      dispatch(action.changeView(nextView));
+    }
+  };
 };
 
 const UserViewContainer = connect(
