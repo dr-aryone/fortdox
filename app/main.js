fb9d89c00c626e2a01bdfa4f1c05a24ee2b8ce03
Lili Du <lilidu@Lilis-MacBook-Pro.local>
2017-07-20 17:02:08 +0200

    CSS fixes on buttons.

--- app/main.js
+++ app/main.js
@@ -1,14 +1,18 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu} = require('electron');
 const path = require('path');
 const urlParser = require('url');
 const querystring = require('querystring');
 const { default: installExtension, REDUX_DEVTOOLS } = require('electron-devtools-installer');
 const config = require('./config.json');
+const {systemPreferences} = require('electron');
+systemPreferences.setUserDefault('NSDisabledDictationMenuItem', 'boolean', true);
+systemPreferences.setUserDefault('NSDisabledCharacterPaletteMenuItem', 'boolean', true);
 let win;
 let activation = {
   type: '',
   code: null
 };
+
 
 async function createWindow() {
   win = new BrowserWindow({width: 1280, height: 720});
@@ -17,9 +21,10 @@ async function createWindow() {
 
   let openingUrl = urlParser.format({
     pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
+    protocol: 'file',
     slashes: true
   });
+
   if (activation.code) {
     switch (activation.type) {
       case 'activate.organization':
@@ -29,11 +34,60 @@ async function createWindow() {
         openingUrl = `${openingUrl}?activateUserCode=${activation.code}`;
     }
   }
+
   win.loadURL(openingUrl);
 
   win.on('closed', () => {
     win = null;
   });
+
+  var template = [{
+    label: 'Application',
+    submenu: [{
+      label: `About ${config.name}`,
+      role: 'orderFrontStandardAboutPanel'
+    }, {
+      label: 'Refresh',
+      accelerator: 'CmdOrCtrl+R',
+      role: 'reload'
+    }, {
+      label: 'Quit',
+      accelerator: 'CmdOrCtrl+Q',
+      click: () => {
+        app.quit();
+      }
+    }]}, {
+    label: 'Edit',
+    submenu: [{
+      label: 'Undo',
+      accelerator: 'CmdOrCtrl+Z',
+      role: 'undo'
+    }, {
+      label: 'Redo',
+      accelerator: 'CmdOrCtrl+Y',
+      role: 'redo'
+    }, {
+      type: 'separator'
+    }, {
+      label: 'Cut',
+      accelerator: 'CmdOrCtrl+X',
+      role: 'cut'
+    }, {
+      label: 'Copy',
+      accelerator: 'CmdOrCtrl+C',
+      role: 'copy'
+    }, {
+      label: 'Paste',
+      accelerator: 'CmdOrCtrl+V',
+      role: 'paste'
+    }, {
+      label: 'Select All',
+      accelerator: 'CmdOrCtrl+A',
+      role: 'selectAll'
+    }]}
+  ];
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 app.setAsDefaultProtocolClient(config.name);
