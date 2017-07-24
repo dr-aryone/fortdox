edd15b28bef5b56222ac665e0dbd24fc580b6979
Lili Du <lilidu@Lilis-MacBook-Pro.local>
2017-07-24 15:41:55 +0200

    Pagination search now scrolls to top

--- app/js/actions/search.js
+++ app/js/actions/search.js
@@ -28,23 +28,18 @@ const setUpdateDocument = id => {
   };
 };
 
-const paginationSearch = index => {
+const search = () => {
   return async (dispatch, getState) => {
     dispatch({
-      type: 'PAGINATION_SEARCH_START'
+      type: 'SEARCH_START'
     });
 
     let state = getState();
-    let searchString;
-    if (index === 0 ) {
-      searchString = state.search.get('searchString');
-      index = 1;
-    }  else {
-      searchString = state.search.get('searchedString');
-    }
+    let searchString = state.search.get('searchString');
     let privateKey = state.user.get('privateKey');
     let organization = state.user.get('organization');
     let email = state.user.get('email');
+    let index = 1;
     let response;
     try {
       response = await requestor.get(`${config.server}/document`, {
@@ -62,18 +57,71 @@ const paginationSearch = index => {
         case 400:
         case 404:
           return dispatch({
-            type: 'PAGINATION_SEARCH_ERROR',
+            type: 'SEARCH_ERROR',
             payload: 'Bad request. Please try again.'
           });
         case 408:
         case 500:
           return dispatch({
-            type: 'PAGINATION_SEARCH_ERROR',
+            type: 'SEARCH_ERROR',
             payload: 'Unable to connect to server. Please try again later.'
           });
       }
     }
-    
+
+    return dispatch({
+      type: 'SEARCH_SUCCESS',
+      payload: {
+        index: index,
+        searchResult: response.body.searchResult,
+        totalHits: response.body.totalHits,
+        searchString: searchString
+      }
+    });
+  };
+};
+
+const paginationSearch = index => {
+  return async (dispatch, getState) => {
+    dispatch({
+      type: 'PAGINATION_SEARCH_START'
+    });
+
+    let state = getState();
+    let searchString = state.search.get('searchedString');
+    let privateKey = state.user.get('privateKey');
+    let organization = state.user.get('organization');
+    let email = state.user.get('email');
+    document.getElementById('top').scrollIntoView();
+    let response;
+    try {
+      response = await requestor.get(`${config.server}/document`, {
+        query: {
+          searchString,
+          organization,
+          email,
+          index
+        },
+        headers: embedPrivateKey(privateKey)
+      });
+    } catch (error) {
+      console.error(error);
+      switch (error.status) {
+        case 400:
+        case 404:
+          return dispatch({
+            type: 'SEARCH_ERROR',
+            payload: 'Bad request. Please try again.'
+          });
+        case 408:
+        case 500:
+          return dispatch({
+            type: 'SEARCH_ERROR',
+            payload: 'Unable to connect to server. Please try again later.'
+          });
+      }
+    }
+
     return dispatch({
       type: 'PAGINATION_SEARCH_SUCCESS',
       payload: {
@@ -86,4 +134,4 @@ const paginationSearch = index => {
   };
 };
 
-module.exports = {setUpdateDocument, paginationSearch};
+module.exports = {setUpdateDocument, search, paginationSearch};
