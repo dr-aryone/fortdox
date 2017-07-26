5b301fbc30c57fc421600a318dce54cc12e1fe11
Johan Callvik <johan@Johans-MacBook-Pro.local>
2017-07-26 15:34:18 +0200

    Now logs in on app open when there is only one user account available

--- app/js/components/front_page/login/LoginView.jsx
+++ app/js/components/front_page/login/LoginView.jsx
@@ -2,54 +2,68 @@ const React = require('react');
 const {readStorage} = require('actions/utilities/storage');
 const config = require('../../../../config.json');
 const MessageBox = require('components/general/MessageBox');
-
-const LoginView = ({loginAs, toRegisterView, toUserView, message}) => {
-  let userList = [];
-  let storage = readStorage();
-  Object.entries(storage).forEach(([email, value]) => {
-    Object.keys(value).forEach((organization) => {
-      userList.push(
-        <div
-          tabIndex='0'
-          onKeyDown={(event) => {
-            if (event.keyCode === 13) loginAs(email, organization, event);
-          }}
-          onClick={(event) => loginAs(email, organization, event)}
-          key={email+organization}
-        >
-          <h2>{organization}</h2>
-          <h3>{email}</h3>
-        </div>
-      );
-    });
-  });
-
-  let concatMessage;
-  if (typeof message === 'object' && message !== null) {
-    concatMessage = [];
-    message.entrySeq().forEach((entry) => {
-      entry[0] === 'bold' ? concatMessage.push(<b key={entry[1]}>{entry[1]}</b>) : concatMessage.push(entry[1]);
-    });
-  } else {
-    concatMessage = message;
+class LoginView extends React.Component {
+  debugger;
+  componentDidMount () {
+    if (this.props.onMount && Object.keys(readStorage()).length === 1) {
+      this.props.onMount(this.props);
+    }
   }
 
-  return (
-    <div className='container'>
-      <MessageBox message={concatMessage} />
-      <h1 className='text-center'>{config.name}</h1>
-      <div className={`box login-panel ${userList.length == 0 ? 'hide' :''}`}>
-        <h2>{userList.length > 0 ? 'Choose an account': null}</h2>
-        {userList}
-      </div>
-      <button onClick={toRegisterView} className='block'>
+  render () {
+    let {
+      loginAs,
+      toRegisterView,
+      toUserView,
+      message
+    } = this.props;
+
+    let userList = [];
+    let storage = readStorage();
+    Object.entries(storage).forEach(([email, value]) => {
+      Object.keys(value).forEach((organization) => {
+        userList.push(
+          <div
+            tabIndex='0'
+            onKeyDown={(event) => {
+              if (event.keyCode === 13) loginAs(email, organization, event);
+            }}
+            onClick={(event) => loginAs(email, organization, event)}
+            key={email+organization}
+          >
+            <h2>{organization}</h2>
+            <h3>{email}</h3>
+          </div>
+        );
+      });
+    });
+    let concatMessage;
+    if (typeof message === 'object' && message !== null) {
+      concatMessage = [];
+      message.entrySeq().forEach((entry) => {
+        entry[0] === 'bold' ? concatMessage.push(<b key={entry[1]}>{entry[1]}</b>) : concatMessage.push(entry[1]);
+      });
+    } else {
+      concatMessage = message;
+    }
+
+    return (
+      <div className='container'>
+        <MessageBox message={concatMessage} />
+        <h1 className='text-center'>{config.name}</h1>
+        <div className={`box login-panel ${userList.length == 0 ? 'hide' :''}`}>
+          <h2>{userList.length > 0 ? 'Choose an account': null}</h2>
+          {userList}
+        </div>
+        <button onClick={toRegisterView} className='block'>
           Register a New Team
-      </button>
-      <button onClick={toUserView} className='block'>
-        Fuskknapp!
-      </button>
-    </div>
-  );
-};
+        </button>
+        <button onClick={toUserView} className='block'>
+          Fuskknapp!
+        </button>
+      </div>
+    );
+  }
+}
 
 module.exports = LoginView;
