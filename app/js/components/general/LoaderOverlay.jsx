defe8d538df7054653538440b33b513302d93bd7
Anton Lövmar <antonlovmar@gmail.com>
2017-08-16 10:12:23 +0200

    Changes the loader color

--- app/js/components/general/LoaderOverlay.jsx
+++ app/js/components/general/LoaderOverlay.jsx
@@ -2,10 +2,11 @@ const React = require('react');
 const Loader = require('components/general/Loader');
 
 const LoginView = ({display}) => {
+  //Primary color for loader, if anyone sets it to 'red', I will find you.
   return (
     <div className={`loader ${display ? 'show' : ''}`}>
       <div className='loader-inner'>
-        <Loader color='#FF0000' />
+        <Loader color='#118AB2' />
       </div>
     </div>
   );
