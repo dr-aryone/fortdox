5a373d4767a346d8507716f7b8a401e5ea764f93
Johan Callvik <johan@Johans-MacBook-Pro.local>
2017-12-18 17:23:41 +0100

    Started work on changelog for documents

---
+++ server/server_modules/changelog/index.js
@@ -0,0 +1,57 @@
+const db = require('app/models');
+
+const get = (documentId) => {
+  return new Promise(async (resolve, reject) => {
+    let doc;
+    try {
+      doc = await db.Changelog.findAll({
+        where: {
+          elasticSearchId: documentId
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      return reject();
+    }
+    return doc;
+  });
+};
+
+
+const addLogEntry = (documentId, user) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await db.Changelog.create({
+        elasticSearchId: documentId,
+        user
+      });
+      return resolve(200);
+    } catch (error) {
+      console.log(error);
+      return reject(500);
+    }
+  });
+};
+
+const remove = (documentId) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await db.Changelog.destroy({
+        where: {
+          elasticSearchId: documentId
+        }
+      });
+      return resolve(200);
+    } catch (error) {
+      console.log(error);
+      return reject(500);
+    }
+  });
+};
+
+
+module.exports = {
+  addLogEntry,
+  get,
+  remove
+};
