6f76fefa2ab357c4eef35feada6ace081de7e234
Peter Nycander <peter.nycander@gmail.com>
2018-02-16 11:22:56 +0100

    Fix bug with copy parsing

--- app/js/components/document/customMarkdown/Copy/styled.js
+++ app/js/components/document/customMarkdown/Copy/styled.js
@@ -1,18 +1,27 @@
 const styled = require('styled-components').default;
 module.exports = {
   Wrapper: styled.span`
-    margin: 0 5px;
-    display: inline-block;
+    margin: 0 2px;
+    height: 1.5rem;
+    display: inline-flex;
+    align-items: center;
     box-shadow: 0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08);
+    max-width: 100%;
   `,
-  Input: styled.input`
-    padding-left: 10px;
+  ValueField: styled.span`
+    padding: 0 10px;
     border: none;
     background: transparent;
     margin: 0;
+    display: flex;
+    align-items: center;
+    line-height: 1.5rem;
+    white-space: nowrap;
+    overflow: hidden;
   `,
   Button: styled.button`
     height: 100%;
+    padding: 0 6px;
     background: hsl(0, 9%, 96%);
     fill: hsla(0, 0%, 35%, 1);
     box-shadow: none;
