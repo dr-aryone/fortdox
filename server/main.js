1f4f0d900a651939b1cddc1d7eae877c993f48fd
David Skeppstedt <david.skeppstedt@gmail.com>
2018-06-11 11:02:05 +0200

    Add CORS module to let react/electron
    Mostly a temporary fix

--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@ const cleanup = require('app/database_cleanup/cleanup');
 const CronJob = require('cron').CronJob;
 const logger = require('app/logger');
 const routes = require('./routes');
+const cors = require('cors');
 const PORT = 8000;
 
 const job = new CronJob('*/30 * * * *', async () => {
@@ -16,8 +17,16 @@ const job = new CronJob('*/30 * * * *', async () => {
 });
 job.start();
 
-app.use(bodyParser.json({limit: '100mb'}));
-app.use('/downloads', (req, res, next) => {console.log(req.originalUrl); next();}, express.static('/opt/fortdox'));
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(
+  '/downloads',
+  (req, res, next) => {
+    console.log(req.originalUrl);
+    next();
+  },
+  express.static('/opt/fortdox')
+);
 app.use('/', routes);
 
 app.listen(PORT, () => {
@@ -28,6 +37,6 @@ app.get('/activation-redirect', (req, res) => {
   res.sendFile(__dirname + '/redirect.html');
 });
 
-app.get('/invite-redirect', (req,res) => {
+app.get('/invite-redirect', (req, res) => {
   res.sendFile(__dirname + '/invite-redirect.html');
 });
