1bb2285c6551a498c272b662259ca6e97ac4d994
David Skeppstedt <david.skeppstedt@gmail.com>
2018-06-19 17:55:27 +0200

    Fix first part of invite new device, it kinda works

--- server/routes/devices.js
+++ server/routes/devices.js
@@ -3,7 +3,7 @@ const devices = require('app/devices');
 const { restrict, needsMasterPassword } = require('app/sessions');
 
 router.get('/', restrict, devices.listDevices);
-// router.post('/add', restrict, needsMasterPassword, devices.add);
+router.post('/add', restrict, needsMasterPassword, devices.add);
 // router.post('/verify', devices.verify);
 // router.post('/confirm', devices.confirm);
 module.exports = router;
