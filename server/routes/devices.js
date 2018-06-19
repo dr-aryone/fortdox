a25c905b57462527241b704ebcbaa2c573c3d146
David Skeppstedt <david.skeppstedt@gmail.com>
2018-06-19 15:46:51 +0200

    Fix listDevices endpoint

--- server/routes/devices.js
+++ server/routes/devices.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const devices = require('app/devices');
-const { restrict } = require('app/sessions');
+const { restrict, needsMasterPassword } = require('app/sessions');
 
 router.get('/', restrict, devices.listDevices);
-
+// router.post('/add', restrict, needsMasterPassword, devices.add);
+// router.post('/verify', devices.verify);
+// router.post('/confirm', devices.confirm);
 module.exports = router;
--- server/server_modules/models/devices.js
+++ server/server_modules/models/devices.js
@@ -1,38 +1,31 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var Devices = sequelize.define(
-    'Devices',
-    {
-      userid: {
-        allowNull: false,
-        type: DataTypes.INTEGER,
-        autoIncrement: false,
-        primaryKey: true
-      },
-      deviceId: {
-        allowNull: false,
-        type: DataTypes.UUID,
-        unique: true,
-        defaultValue: DataTypes.UUIDV4
-      },
-      password: {
-        allowNull: true,
-        type: DataTypes.BLOB
-      },
-      deviceName: { type: DataTypes.STRING },
-      activated: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-      }
+  var Devices = sequelize.define('Devices', {
+    userid: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      autoIncrement: false,
+      primaryKey: true
     },
-    {
-      classMethods: {
-        associate: function(models) {
-          // associations can be defined here
-          Devices.belongsTo(models.User, { foreignKey: 'userid' });
-        }
-      }
+    deviceId: {
+      allowNull: false,
+      type: DataTypes.UUID,
+      unique: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    password: {
+      allowNull: true,
+      type: DataTypes.BLOB
+    },
+    deviceName: { type: DataTypes.STRING },
+    activated: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     }
-  );
+  });
+  Devices.associate = models => {
+    Devices.belongsTo(models.User, { foreignKey: 'userid' });
+  };
+
   return Devices;
 };
