9e3b2f15010ee10c0077d6bfc118c9cf4693b645
David Skeppstedt <david.skeppstedt@gmail.com>
2018-06-19 10:00:31 +0200

    Update invite route to use the needsMasterPasssword middleware

--- server/routes/invite.js
+++ server/routes/invite.js
@@ -1,10 +1,10 @@
 const invite = require('app/invite');
 const express = require('express');
 const router = express.Router();
-const {restrict} = require('app/sessions');
+const { restrict, needsMasterPassword } = require('app/sessions');
 
 router.post('/confirm', invite.confirm);
 router.post('/verify', invite.verify);
-router.post('/', restrict, invite.user);
+router.post('/', restrict, needsMasterPassword, invite.user);
 
 module.exports = router;
