1762ff48dd3964f51908bfb7875737380a523867
David Skeppstedt <david.skeppstedt@gmail.com>
2018-07-13 10:30:51 +0200

    Add testrunner

---
+++ server/tests/integrations_tests/testrunner.js
@@ -0,0 +1,41 @@
+const flags = require('flags');
+const { execSync } = require('child_process');
+const registerTest = require('./registration.test');
+const loginTest = require('./login.test');
+const deviceTest = require('./device.test');
+
+flags.defineBoolean('register', false);
+flags.defineBoolean('clean', false);
+flags.parse();
+
+run();
+
+function run() {
+  const runRegister = flags.get('register');
+  const runClean = flags.get('clean');
+
+  console.log(
+    '##################### INTEGRATION TESTING COMMENCING ######################'
+  );
+
+  if (runClean || runRegister) {
+    clean();
+    if (runClean) {
+      process.exit();
+    }
+  }
+
+  if (runRegister) {
+    registerTest
+      .run()
+      .then(loginTest.run)
+      .then(deviceTest.run);
+  } else {
+    loginTest.run().then(deviceTest.run);
+  }
+}
+
+function clean() {
+  const buffer = execSync('node dev_cleanup.test.js');
+  console.log(String(buffer));
+}
