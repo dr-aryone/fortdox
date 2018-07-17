b25d4bc3f313ad9ea1057671ddef042d8b9faa75
David Skeppstedt <david.skeppstedt@gmail.com>
2018-07-17 13:20:03 +0200

    Fix versionMiddleware

--- server/versionMiddleware.js
+++ server/versionMiddleware.js
@@ -5,19 +5,15 @@ const checkVersion = function(req, res, next) {
     return;
   }
 
-  const version = req.get('X-FORTDOX-VERSION');
+  const version = req.get('x-fortdox-version');
 
   if (correctVersion(version)) {
     next();
   } else {
-    res.set('X-FORTDOX-REQUIRED-VERSION', config.clientVersion);
+    res.set('x-fortdox-required-version', config.clientVersion);
     res.statusCode = 400;
     res.send({
-      message: `
-The version of your client is no longer supported, please update to ${
-  config.clientVersion
-}.
-    If you need help please contact the FortDox developers.`
+      message: `Unsported version, please use ${config.clientVersion}`
     });
     res.end();
   }
