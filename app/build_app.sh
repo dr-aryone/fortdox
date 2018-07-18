1b86a54a19b6ce7b8631e03fe06dcac033de46de
David Skeppstedt <david.skeppstedt@gmail.com>
2018-07-18 16:53:36 +0200

    Fix filename is not fixed to test.zip instead read from config.json.
    Also build_app.sh also prompts for a version number and builds fortdox-versionumber.zip files for distrubtion

--- app/build_app.sh
+++ app/build_app.sh
@@ -1,5 +1,6 @@
 npm run build
 electron-packager . --icon public/resources/logo.icns --app-bundle-id com.edgeguide.fortdox --overwrite
 codesign --deep --force --verbose --sign "Mac Developer: David Skeppstedt (SQM8QGLKG8)" FortDox-darwin-x64/FortDox.app
-zip -9 -r FortDox-darwin-x64/test.zip FortDox-darwin-x64/FortDox.app/
+read -p "Enter version number, e.g 1.0: " versionNumber
+zip -9 -q -r FortDox-darwin-x64/fortdox-$versionNumber.zip FortDox-darwin-x64/FortDox.app/
 echo 'Done!'
