b5aa7b74a533bf29daf2b8fae763ab6232e86a8a
David Skeppstedt <david.skeppstedt@gmail.com>
2018-07-18 12:01:27 +0200

    Update versionMiddleware to let /update pass

--- server/versionMiddleware.js
+++ server/versionMiddleware.js
@@ -1,19 +1,21 @@
+const logger = require('app/logger');
 const config = require('app/config');
+
 const checkVersion = function(req, res, next) {
-  if (req.method.toUpperCase() === 'OPTIONS') {
+  if (req.method.toUpperCase() === 'OPTIONS' || req.path.includes('update')) {
     next();
     return;
   }
 
   const version = req.get('x-fortdox-version');
-
   if (correctVersion(version)) {
     next();
   } else {
+    logger.info('version', 'Client with outdated version');
     res.set('x-fortdox-required-version', config.clientVersion);
     res.statusCode = 400;
     res.send({
-      message: `Unsported version, please use ${config.clientVersion}`
+      message: `Unsupported version, please use ${config.clientVersion}`
     });
     res.end();
   }
