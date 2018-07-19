fbfc7ffce94dba5002b300dec5350f87e8f86603
Lili Du <lilid@kth.se>
2018-07-19 13:54:28 +0200

    Previous user should now log out when clicing on a invite/register link

--- app/main.js
+++ app/main.js
@@ -80,7 +80,7 @@ function createBrowserWindow() {
         console.log(`Added Extension:  ${name}`);
       })
       .catch(err => {
-        console.log('An error occurred: ', err);
+        console.error('An error occurred: ', err);
       });
   }
   openWindow = true;
