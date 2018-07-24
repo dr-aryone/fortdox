7371b1f4b5edd101c03c065818f3f141737e3a23
David Skeppstedt <david.skeppstedt@gmail.com>
2018-07-24 15:02:49 +0200

    Add warn logger file and set default to info

--- server/server_modules/logger/index.js
+++ server/server_modules/logger/index.js
@@ -1,27 +1,33 @@
 const winston = require('winston');
 
-const logger = new (winston.Logger)({
+const logger = new winston.Logger({
   transports: [
-    new (winston.transports.File)({
+    new winston.transports.File({
       name: 'info-file',
       filename: __dirname + '/../../logs/filelog-info.log',
       level: 'info',
       timestamps: true
     }),
-    new (winston.transports.File)({
+    new winston.transports.File({
       name: 'error-file',
       filename: __dirname + '/../../logs/filelog-error.log',
       level: 'error',
       timestamps: true
     }),
-    new (winston.transports.File)({
+    new winston.transports.File({
+      name: 'warn-file',
+      filename: __dirname + '/../../logs/filelog-warn.log',
+      level: 'warn',
+      timestamps: true
+    }),
+    new winston.transports.File({
       name: 'silly-file',
       filename: __dirname + '/../../logs/filelog-silly.log',
       level: 'silly',
       timestamps: true
     }),
-    new (winston.transports.Console)({
-      level: 'silly',
+    new winston.transports.Console({
+      level: 'info',
       timestamps: true
     })
   ]
