f62b8d9562f2795e0729d18e68f99a926d8649b7
David Skeppstedt <david.skeppstedt@gmail.com>
2018-07-26 11:32:23 +0200

    Refactor get attachment handler and endpoint
    DOX-155

--- server/routes/document.js
+++ server/routes/document.js
@@ -8,7 +8,11 @@ router.post('/', needsMasterPassword, document.create);
 router.get('/:id', needsMasterPassword, document.get);
 router.delete('/:id', needsMasterPassword, document.delete);
 router.patch('/:id', needsMasterPassword, document.update);
-router.get('/:id/attachment/:attachmentIndex', document.getAttachment);
 router.get('/check/title', document.checkTitle);
+
+//Attachment routes
+const attachments = require('app/attachments');
+router.use('/:id/attachment', attachments);
+//router.get('/:id/attachment/:attachmentIndex', document.getAttachment);
 
 module.exports = router;
---
+++ server/server_modules/attachments/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const getAttachment = require('./attachment.get');
+
+router.get('/:attachmentIndex', getAttachment);
+
+module.exports = router;
