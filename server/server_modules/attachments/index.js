117ca02e39ced5d5fef030e9bebabd2274561058
David Skeppstedt <david.skeppstedt@gmail.com>
2018-07-30 16:13:18 +0200

    Remove attachment upload endpoint handler Not needed

--- server/server_modules/attachments/index.js
+++ server/server_modules/attachments/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const getAttachment = require('./attachment.get');
-const uploadAttachment = require('./attachment.post');
-
 router.get('/:attachmentIndex', getAttachment);
-//router.post('/', upload.array('attachments[]'), uploadAttachment);
 
 module.exports = router;
