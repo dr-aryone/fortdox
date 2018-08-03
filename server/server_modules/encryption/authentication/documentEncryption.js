1fb50ec0913d864174be60d1eb751d7e39ea443c
David Skeppstedt <david.skeppstedt@gmail.com>
2018-08-03 15:23:27 +0200

    Fix get documents to decrypt all encrypted texts in all versions

--- server/server_modules/encryption/authentication/documentEncryption.js
+++ server/server_modules/encryption/authentication/documentEncryption.js
@@ -1,11 +1,18 @@
 const cryptMP = require('../keys/cryptMasterPassword.js');
 const aes = require('./aes');
 
-const encryptDocument = (encryptedTexts, privateKey, encryptedMasterPassword) => {
+const encryptDocument = (
+  encryptedTexts,
+  privateKey,
+  encryptedMasterPassword
+) => {
   return new Promise(async (resolve, reject) => {
     let masterPassword;
     try {
-      masterPassword = await cryptMP.decryptMasterPassword(privateKey, encryptedMasterPassword);
+      masterPassword = await cryptMP.decryptMasterPassword(
+        privateKey,
+        encryptedMasterPassword
+      );
     } catch (error) {
       return reject(400);
     }
@@ -22,25 +29,36 @@ const encryptDocument = (encryptedTexts, privateKey, encryptedMasterPassword) =>
   });
 };
 
-const decryptDocuments = (encryptedTexts , privateKey, encryptedMasterPassword) => {
+const decryptDocuments = (
+  encryptedTexts,
+  privateKey,
+  encryptedMasterPassword
+) => {
   return new Promise(async (resolve, reject) => {
     let masterPassword;
     try {
-      masterPassword = await cryptMP.decryptMasterPassword(privateKey, encryptedMasterPassword);
+      masterPassword = await cryptMP.decryptMasterPassword(
+        privateKey,
+        encryptedMasterPassword
+      );
     } catch (error) {
       console.error(error);
       return reject(500);
     }
     try {
       for (let textField of encryptedTexts) {
-        let text = await aes.decrypt(masterPassword, Buffer.from(textField.text, 'base64'));
+        let text = await aes.decrypt(
+          masterPassword,
+          Buffer.from(textField.text, 'base64')
+        );
         textField.text = text.toString();
       }
       return resolve(encryptedTexts);
     } catch (error) {
+      console.error(error);
       reject(500);
     }
   });
 };
 
-module.exports = {encryptDocument, decryptDocuments};
+module.exports = { encryptDocument, decryptDocuments };
