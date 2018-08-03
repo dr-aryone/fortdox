83a13c659e593c5f2cc4b49770c3abdaed0539fe
Lili Du <lilid@kth.se>
2018-08-03 16:27:42 +0200

    Updated version panel.

--- app/src/components/document/form/VersionHistory.jsx
+++ app/src/components/document/form/VersionHistory.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+const { formatDate } = require('components/general/formatDate');
 
 class VersionHistory extends Component {
   constructor(props) {
@@ -7,76 +8,32 @@ class VersionHistory extends Component {
   }
 
   render() {
-    const { versions } = this.props;
-    debugger;
+    const { versions, insertDocumentVersion, onToggleVersionPanel } = this.props;
+    const versionList = versions.map((version, index) => {
+      return (
+        <div
+          className={`version-item ${!version.get('title') ? 'disabled' : ''}`}
+          key={index}
+          onClick={() => {
+            if (version.get('title')) insertDocumentVersion(version);
+          }}
+        >
+          <h3>{formatDate(version.get('createdAt'))}</h3>
+          <p>{index === versions.size - 1 ? 'Current' : null}</p>
+          <p>{version.get('user')}</p>
+        </div>
+      );
+    });
+
     return (
       <div className='version-history-panel'>
         <div className='version-header'>
           <h2>Version History</h2>
+          <span className='close' onClick={() => onToggleVersionPanel(false)}>
+            Close
+          </span>
         </div>
-        <div className='version-list'>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>Current</p>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-          <div className='version-item'>
-            <h3>2 August, 2018</h3>
-            <p>liliduxx@gmail.com</p>
-          </div>
-        </div>
+        <div className='version-list'>{versionList.reverse()}</div>
       </div>
     );
   }
