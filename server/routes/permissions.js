9ba149ce87d62d5ab0ce265455c6a575857c91da
David Skeppstedt <david.skeppstedt@gmail.com>
2018-08-07 10:12:23 +0200

    Add require grant permission middleware

--- server/routes/permissions.js
+++ server/routes/permissions.js
@@ -3,8 +3,15 @@ const router = express.Router();
 const readUsersAndTheirPermissions = require('app/permissions/permissions.get');
 const updateUserPermission = require('app/permissions/permissions.post');
 const { restrict } = require('app/sessions');
+const requiresGrantPermission = require('app/permissions/grantPermissionMiddleware');
 
-router.get('/', restrict, readUsersAndTheirPermissions);
-router.post('/', restrict, updateUserPermission);
+router.get(
+  '/',
+  restrict,
+  requiresGrantPermission,
+  readUsersAndTheirPermissions
+);
+
+router.post('/', restrict, requiresGrantPermission, updateUserPermission);
 
 module.exports = router;
