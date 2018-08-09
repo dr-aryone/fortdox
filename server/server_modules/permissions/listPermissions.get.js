3622f6c12f8904253555ec59ddafbcb1b76d6e11
David Skeppstedt <david.skeppstedt@gmail.com>
2018-08-09 10:54:29 +0200

    Fix /permissions to send correct permission set depending on if user is owner or not

--- server/server_modules/permissions/listPermissions.get.js
+++ server/server_modules/permissions/listPermissions.get.js
@@ -1,12 +1,45 @@
 const permissions = require('app/permissions');
 const logger = require('app/logger');
+const db = require('app/models');
 
-function listPermissions(req, res) {
+async function listPermissions(req, res) {
   logger.info(
     '/permissions/',
     `${req.session.email} checking what permissions exist`
   );
-  res.send(permissions.names);
+
+  let listablePermissions = Object.entries(permissions.names).filter(
+    n => n[1] !== permissions.getPermissionName(permissions.BASE)
+  );
+
+  let isOwner = false;
+  try {
+    const email = req.session.email;
+    const user = await db.User.findOne({ where: { email: email } });
+    if (!user) {
+      logger.error('/permissions/me', `Did not finde user ${email}`);
+      return res.status(500).send();
+    }
+    const orgOwner = await db.Organization.findOne({
+      where: { id: req.session.organizationId, owner: user.id }
+    });
+    isOwner = orgOwner ? true : undefined;
+  } catch (error) {
+    logger.error(error);
+    return res.status(500).send();
+  }
+
+  if (!isOwner)
+    listablePermissions = listablePermissions.filter(
+      p => p[1] !== permissions.getPermissionName(permissions.GRANT_PERMISSION)
+    );
+
+  listablePermissions = listablePermissions.reduce((result, item) => {
+    result[item[0]] = item[1];
+    return result;
+  }, {});
+
+  res.send(listablePermissions);
 }
 
 module.exports = listPermissions;
