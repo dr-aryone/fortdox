d692b664d057e482fe0b4370c2da499df6407e7a
David Skeppstedt <david.skeppstedt@gmail.com>
2018-08-09 13:48:02 +0200

    Fix that allows permission managers to manage their own permissions

--- server/server_modules/permissions/permissions.post.js
+++ server/server_modules/permissions/permissions.post.js
@@ -2,6 +2,7 @@ const expect = require('@edgeguideab/expect');
 const logger = require('app/logger');
 const db = require('app/models');
 const acu = require('./accessControlUnit');
+const permissions = require('app/permissions');
 
 async function updateUserPermission(req, res) {
   const expectations = expect(
@@ -18,8 +19,34 @@ async function updateUserPermission(req, res) {
       .end();
   }
 
-  const newPermission = req.body.permission;
+  let newPermission = req.body.permission;
   const userEmail = req.body.email;
+
+  const permissionManagerIsUpdatingSelf = req.session.email === userEmail;
+  const newPermissionIncludesGrant =
+    req.body.permission &
+    (permissions.GRANT_PERMISSION === permissions.GRANT_PERMISSION);
+
+  if (
+    permissionManagerIsUpdatingSelf &&
+    req.session.permission === newPermission
+  ) {
+    logger.info(
+      '/permissions POST',
+      'Permission manager asked to set its own permission to the same value, i.e a noop.'
+    );
+    return res.send();
+  }
+
+  if (permissionManagerIsUpdatingSelf && newPermissionIncludesGrant) {
+    logger.info(
+      '/permissions POST',
+      `Permission manager: ${
+        req.session.email
+      } is setting itself to permission ${newPermission}`
+    );
+    newPermission = newPermission & ~permissions.GRANT_PERMISSION;
+  }
 
   if (!acu(req.session.permission).canSet(newPermission)) {
     logger.warn(
@@ -29,6 +56,15 @@ async function updateUserPermission(req, res) {
       } tried to grant permission ${newPermission} to ${userEmail}`
     );
     return res.status(400).send();
+  }
+
+  if (
+    (req.session.permission & permissions.GRANT_PERMISSION) ===
+      permissions.GRANT_PERMISSION &&
+    permissionManagerIsUpdatingSelf &&
+    newPermissionIncludesGrant
+  ) {
+    newPermission = newPermission | permissions.GRANT_PERMISSION;
   }
 
   try {
