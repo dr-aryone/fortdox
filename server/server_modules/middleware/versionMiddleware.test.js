b666453d3d24d48e292973503b7ca23686637d75
Anton Lövmar <antonlovmar@gmail.com>
2018-08-14 18:39:01 +0200

    Changes version checking to allow all versions higher than the minimum

--- server/server_modules/middleware/versionMiddleware.test.js
+++ server/server_modules/middleware/versionMiddleware.test.js
@@ -1,21 +1,74 @@
-const { correctVersion } = require('./versionMiddleware');
-const config = require('app/config');
+let correctVersion;
+
+beforeEach(() => {
+  jest.resetModules();
+  jest.resetAllMocks();
+});
+
+function setup(mockedVersion) {
+  jest.doMock('app/config', () => ({
+    clientVersion: mockedVersion
+  }));
+  correctVersion = require('./versionMiddleware').correctVersion;
+}
 
 test('If no header it fails', () => {
+  setup('1.0.0');
   const version = '';
   expect(correctVersion(version)).toBe(false);
 });
 
-test('Present header but version is lower than required should fail', () => {
+test('If a badly formatted is provided header it fails (1.a.3)', () => {
+  setup('1.0.0');
+  const version = '1.a.3';
+  expect(correctVersion(version)).toBe(false);
+});
+
+test('If a badly formatted is provided header it fails (a.b.1)', () => {
+  setup('1.0.0');
+  const version = 'a.b.1';
+  expect(correctVersion(version)).toBe(false);
+});
+
+test('If a badly formatted is provided header it fails (foobar)', () => {
+  setup('1.0.0');
+  const version = 'foobar';
+  expect(correctVersion(version)).toBe(false);
+});
+
+test('Present header but major version is lower than required should fail', () => {
+  setup('1.0.0');
   const version = '0.5';
   expect(correctVersion(version)).toBe(false);
 });
-test('Present header but version is higher than required should fail', () => {
+
+test('Present header but minor version is lower than required should fail', () => {
+  setup('1.2.1');
   const version = '1.2';
   expect(correctVersion(version)).toBe(false);
 });
 
+test('Present header but minor version is higher than required should pass', () => {
+  setup('1.0.0');
+  const version = '1.2';
+  expect(correctVersion(version)).toBe(true);
+});
+
+test('Present header but minor version is higher than required should pass', () => {
+  setup('1.1.1');
+  const version = '1.2';
+  expect(correctVersion(version)).toBe(true);
+});
+
+test('Present header but major version is higher than required should pass', () => {
+  setup('1.1.1');
+  const version = '2.1';
+  expect(correctVersion(version)).toBe(true);
+});
+
 test('Present header and version is matching it should pass', () => {
-  const version = config.clientVersion;
+  setup('1.0.0');
+  const version = '1.0.0';
+
   expect(correctVersion(version)).toBe(true);
 });
